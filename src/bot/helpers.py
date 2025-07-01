e922c8368097245718230c33f32998c4b4ceaa40
Kozimjon <developer.0829@gmail.com>
2025-07-01 14:29:48 +0500

    quick fix

--- src/bot/helpers.py
+++ src/bot/helpers.py
@@ -4,7 +4,7 @@ from django.conf import settings
 
 
 def get_bot_webhook_url():
-    return f"{settings.BASE_URL}/v1/bot/webhook"
+    return f"{settings.BASE_URL}/v1/bot/webhook/"
 
 
 def validate_phone_number(phone_number, region='UZ'):
